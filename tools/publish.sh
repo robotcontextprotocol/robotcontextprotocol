2927ba33a2d4b3de2f421deb1984a7d9ba64e9d5
MakerYang <open@wileho.com>
2025-04-21 13:43:45 +0800

    init

--- tools/publish.sh
+++ tools/publish.sh
@@ -15,4 +15,46 @@
 
 set -e
 
+# Get the linux arch info
+architecture=$(dpkg --print-architecture)
+
+# Get the linux standard base
 lsb_release -a
+
+# Get the version
+version=$(grep 'Version:.*"' "../backend/pkg/version/version.go" | awk -F'"' '{print $2}')
+
+# Get the current time
+datetime=$(date +%Y%m%d%H%M%S)
+
+# Generate debian package
+if [ ! -d "debian" ]; then
+    mkdir -p debian
+    sudo cp -r ubuntu/* ./debian/
+    sudo chmod +x debian/DEBIAN/*
+    find ./debian -type f -name ".gitkeep" -exec rm -f {} +
+else
+    sudo rm -rf debian && sudo rm -rf ./*.deb && mkdir -p debian
+    sudo cp -r ubuntu/* ./debian/
+    sudo chmod +x debian/DEBIAN/*
+    find ./debian -type f -name ".gitkeep" -exec rm -f {} +
+fi
+
+# Copy xmake files to debian package
+sudo cp /usr/local/bin/xmake ./debian/usr/local/bin/
+sudo cp -r /usr/local/share/xmake/* ./debian/usr/local/share/xmake/
+
+# Copy stlink files to debian package
+sudo cp -r /usr/local/lib/libstlink* ./debian/usr/local/lib/
+sudo cp -r /usr/local/bin/st-* ./debian/usr/local/bin/
+sudo cp -r /usr/local/bin/st-* ./debian/usr/local/bin/
+sudo cp /etc/modprobe.d/stlink_v1.conf ./debian/etc/modprobe.d/
+sudo cp -r /lib/udev/rules.d/49-stlinkv* ./debian/lib/udev/rules.d/
+sudo cp -r /usr/local/share/stlink/* ./debian/usr/local/share/stlink/
+sudo cp -r /usr/local/include/stlink/* ./debian/usr/local/include/stlink/
+sudo cp -r /usr/local/share/man/man1/st-* ./debian/usr/local/share/man/man1/
+
+/usr/local/go/bin/go env -w GOSUMDB=off
+/usr/local/go/bin/go env -w GOPATH=/tmp/golang
+/usr/local/go/bin/go env -w GOMODCACHE=/tmp/golang/pkg/mod
+export GO111MODULE=on && export GOPROXY=https://goproxy.io
